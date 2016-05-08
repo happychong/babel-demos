91fa7b2839cb65d46984a1dc4a69bee23df49717
happychong <1173290465@qq.com>
2016-05-09 00:42:48 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import "babel-polyfill";
+import 'babel-polyfill';
 
-console.log('======')
+console.log(Array.from('aaa'));
+
+const sum = (a, b) => a + b;
+let x = sum(1, 2);
+let y = sum(2, 2);
+
+console.log(x);
---
+++ src/jsx.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class DemoCompinent extends Component {
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
